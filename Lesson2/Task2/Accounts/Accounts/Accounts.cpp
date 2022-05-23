81e8c19502caa8740de79adb6cb3e770824edc55
Grushaas <grusha_as@outlook.com>
2022-05-24 00:16:28 +0500

    Lesson 2

---
+++ Lesson2/Task2/Accounts/Accounts/Accounts.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+//Счет
+struct Account {
+	int NumberAccount = 0;
+	std::string name;
+	double bank = 0;
+};
+
+void accountUpdate(Account& ac) {
+	//Функция для изменения баланса
+	
+	int bank = 0;
+	std::cout << "Введите новый баланс: ";
+	std::cin >> bank;
+	
+	ac.bank = bank;
+}
+
+int main() {
+	//Русификатор
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int numAc = 0;
+	std::string name;
+	double bank = 0;
+
+	//Данные счета
+	std::cout << "Введите номер счета: ";
+	std::cin >> numAc;
+	std::cout << "Введите имя владельца: ";
+	std::cin >> name;
+	std::cout << "Введите баланс: ";
+	std::cin >> bank;
+	
+	//Заполнение структуры
+	Account p;
+	Account* p_p = &p;
+	p_p->NumberAccount = numAc;
+	p_p->name = name;
+	p_p->bank = bank;
+
+	accountUpdate(p);
+
+	//Вывод структуры
+	std::cout << "Ваш счет: " << p.name << ", " << p.NumberAccount << ", " << p.bank;
+}
---
+++ Lesson2/Task3/StructureOutput/StructureOutput/StructureOutput.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+//Структура адресов
+struct address {
+	std::string country = "Россия";
+	std::string town = "Тюмень";
+	std::string street = "Дмитрия Менделеева";
+	int houseNumber = 19;
+	int apartmentNumber = 151;
+	int index = 611251;
+};
+
+void outputStruct(address& addr) {
+	//Вывод структуры
+	std::cout << "Страна: " << addr.country << std::endl;
+	std::cout << "Город: " << addr.town << std::endl;
+	std::cout << "Улица: " << addr.street << std::endl;
+	std::cout << "Номер дома: " << addr.houseNumber << std::endl;
+	std::cout << "Номер квартиры: " << addr.apartmentNumber << std::endl;
+	std::cout << "Индекс: " << addr.index << std::endl;
+}
+
+int main() {
+	//Руссификатор
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	address addr;
+	outputStruct(addr);
+}
