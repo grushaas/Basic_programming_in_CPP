355f68f5ea172204d5879b2245632c82086a99eb
Grushaas <grusha_as@outlook.com>
2022-05-23 16:56:51 +0500

    Task1

---
+++ Lesson 1/Task 1/Task1/Task1/Task1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main() {
+	std::ifstream in ("in.txt");
+	int size1 = 0;
+	int size2 = 0;
+
+	in >> size1;
+
+	int* arr = new int[size1]();
+	for (int i = 0; i < size1; ++i) {
+		int value = 0;
+		in >> value;
+		arr[i] = value;
+	}
+
+	in >> size2;
+	std::cout << size2 << std::endl;
+	int* arr2 = new int[size2]();
+	for (int i = 0; i < size2; ++i) {
+		int value = 0;
+		in >> value;
+		arr2[i] = value;
+	}
+
+	in.close();
+
+	std::ofstream fout("out.txt");
+	fout << size2 << std::endl;
+	for (int i = size2; size2 > 0; --i) {
+		int value = 0;
+		value = arr2[i];
+		fout << value;
+	}
+	std::cout << std::endl;
+
+	fout << size1 << std::endl;
+	for (int i = size1; size1 > 0; --i) {
+		int value = 0;
+		value = arr[i];
+		fout << value;
+	}
+	std::cout << std::endl;
+
+	fout.close();
+
+	delete[] arr;
+	delete[] arr2;
+}
