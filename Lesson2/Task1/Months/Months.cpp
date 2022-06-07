9a2b06826a9131b440612cd45123b240d0192835
Grushaas <grusha_as@outlook.com>
2022-06-07 15:55:21 +0500

    Version 1.2 - Task 1

---
+++ Lesson2/Task1/Months/Months.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+
+enum class Seasons {
+    
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+
+int main() {
+    int value = 1;
+
+    while (value != 0) {
+        std::cout << "Enter month number: ";
+        std::cin >> value;
+
+        Seasons season = static_cast<Seasons>(value);
+
+        if (value > 12) {
+            std::cout << "Wrong number!" << std::endl;
+        }
+        else if (value < 0) {
+            std::cout << "Wrong number!" << std::endl;
+        }
+
+        switch (static_cast<Seasons>(value)) {
+        case Seasons::January:
+            std::cout << "January" << std::endl;
+            break;
+        case Seasons::February:
+            std::cout << "February" << std::endl;
+            break;
+        case Seasons::March:
+            std::cout << "March" << std::endl;
+            break;
+        case Seasons::April:
+            std::cout << "April" << std::endl;
+            break;
+        case Seasons::May:
+            std::cout << "May" << std::endl;
+            break;
+        case Seasons::June:
+            std::cout << "June" << std::endl;
+            break;
+        case Seasons::July:
+            std::cout << "July" << std::endl;
+            break;
+        case Seasons::August:
+            std::cout << "August" << std::endl;
+            break;
+        case Seasons::September:
+            std::cout << "September" << std::endl;
+            break;
+        case Seasons::October:
+            std::cout << "October" << std::endl;
+            break;
+        case Seasons::November:
+            std::cout << "November" << std::endl;
+            break;
+        case Seasons::December:
+            std::cout << "December" << std::endl;
+            break;
+        }
+    }
+
+    std::cout << "До свидания" << std::endl;
+}
+
+
