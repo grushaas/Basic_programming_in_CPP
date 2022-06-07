977ce6af25ed67ba13d036a619e1e51e945c81c9
Grushaas <grusha_as@outlook.com>
2022-06-07 16:08:52 +0500

    Version 1.4 - Task 1

--- Lesson3/Task1/CalculatorForTwoNumbers/CalculatorForTwoNumbers.cpp
+++ Lesson3/Task1/CalculatorForTwoNumbers/CalculatorForTwoNumbers.cpp
@@ -63,49 +63,8 @@ int main() {
 	double num1;
 	double num2;
 
-	do {
-		std::cout << "Введите num1: ";
-		std::cin >> num1;
-		if(calc.set_num1(num1)) {
-			break;
-		}
-		else {
-			std::cout << "Неверный ввод!" << std::endl;
-		}
-	} while(true);
-
-	do {
-		std::cout << "Введите num2: ";
-		std::cin >> num2;
-		if(calc.set_num2(num2)) {
-			break;
-		}
-		else {
-			std::cout << "Неверный ввод!" << std::endl;
-		}
-	} while(true);
-
 	while (true) {
-		//Сложение
-		std::cout << "num1 + num2 = " << calc.add() << std::endl;
-
-		//Вычитания num2 из num1
-		std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
-
-		//Вычитания num1 из num2
-		std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
-
-		//Умножение
-		std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
-
-		//Деление num1 на num2
-		std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
-
-		//Деление num2 на num1
-		std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
-
-		//Смена числа - num1 и num2
-		
+		// Смена числа num1 и num2
 		do {
 			std::cout << "Введите num1: ";
 			std::cin >> num1;
@@ -127,6 +86,24 @@ int main() {
 				std::cout << "Неверный ввод!" << std::endl;
 			}
 		} while (true);
+
+		//Сложение
+		std::cout << "num1 + num2 = " << calc.add() << std::endl;
+
+		//Вычитания num2 из num1
+		std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
+
+		//Вычитания num1 из num2
+		std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
+
+		//Умножение
+		std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+
+		//Деление num1 на num2
+		std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+
+		//Деление num2 на num1
+		std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
 	}
 
 	return 0;
