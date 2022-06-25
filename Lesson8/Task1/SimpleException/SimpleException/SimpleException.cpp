4ed70b0506d17f242a6836318ff5e7263247a80f
Grushaas <grusha_as@outlook.com>
2022-06-25 23:52:16 +0700

    Lesson 8 - Task 1 - Version 1.0

---
+++ Lesson8/Task1/SimpleException/SimpleException/SimpleException.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+class ForbiddenWordLength : public std::exception
+{
+public:
+	const char* what() const override { return "Запретная длина слова. До свидания!"; }
+};
+
+int function(std::string str, int forbiddenLength)
+{
+	if (str.length() == forbiddenLength) throw ForbiddenWordLength();
+	return str.length();
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	std::string str;
+	int forbiddenLength = 0;
+
+	std::cout << "Введите запретную длину: ";
+	std::cin >> forbiddenLength;
+	while (str.length() != forbiddenLength)
+	{
+		std::cout << "Введите слово: ";
+		std::cin >> str;
+		try
+		{
+			std::cout << "Длина слова " << str << " равна " << function(str, forbiddenLength) << std::endl;
+		}
+		catch (const ForbiddenWordLength& forbidden)
+		{
+			std::cout << forbidden.what() << std::endl;
+		}
+		catch (...)
+		{
+			std::cout << "Неизвестная ошибка" << std::endl;
+		}
+	}
+
+
+	return 0;
+}
