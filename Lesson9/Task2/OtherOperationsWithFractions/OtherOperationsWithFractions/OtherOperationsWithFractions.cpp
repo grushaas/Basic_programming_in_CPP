2f93200683f23cd78dca375211af2ecc940bd76d
Grushaas <grusha_as@outlook.com>
2022-07-03 16:42:53 +0700

    Lesson 9 - Task 2 - Version 1.1

--- Lesson9/Task2/OtherOperationsWithFractions/OtherOperationsWithFractions/OtherOperationsWithFractions.cpp
+++ Lesson9/Task2/OtherOperationsWithFractions/OtherOperationsWithFractions/OtherOperationsWithFractions.cpp
@@ -1,10 +1,31 @@
 ﻿#include <iostream>
+#include <string>
 
 class Fraction
 {
 private:
 	int numerator_;
 	int denominator_;
+
+	void reduction(int& numerator, int& denominator)
+	{
+		int nod = 0;
+
+		while (nod != 1)
+		{
+			for (int i = numerator; i > 0; i--)
+			{
+				if (numerator % i == 0 && denominator % i == 0)
+				{
+					nod = i;
+					break;
+				}
+			}
+
+			numerator /= nod;
+			denominator /= nod;
+		}
+	}
 
 public:
 	Fraction(int numerator, int denominator)
@@ -13,83 +34,160 @@ public:
 		denominator_ = denominator;
 	}
 
-	double Abs()
+	Fraction operator+(Fraction frac)
 	{
-		return std::sqrt(numerator_ * numerator_ + denominator_ * denominator_);
+		if (denominator_ == frac.denominator_)
+		{
+			int numerator = numerator_ + frac.numerator_;
+			return Fraction(numerator, denominator_);
+		}
+
+		int stn = denominator_ * frac.denominator_;
+		int numerator1 = frac.denominator_ * numerator_;
+		int numerator2 = denominator_ * frac.numerator_;
+		int numerator = numerator1 + numerator2;
+		
+		return Fraction(numerator, stn);
 	}
 
-	Fraction operator+(Fraction other)
+	Fraction operator-(Fraction frac)
 	{
-		return Fraction(numerator_ + other.numerator_, denominator_ + other.denominator_);
-	}
-	Fraction operator+(int other)
-	{
-		return Fraction(numerator_ + other, denominator_);
-	}
-	Fraction operator-(Fraction other)
-	{
-		return Fraction(numerator_ - other.numerator_, denominator_ - other.denominator_);
-	}
-	Fraction operator-(int other)
-	{
-		return Fraction(numerator_ - other, denominator_);
+		if (denominator_ == frac.denominator_)
+		{
+			int numerator = numerator_ - frac.numerator_;
+			return Fraction(numerator, denominator_);
+		}
+
+		int stn = denominator_ * frac.denominator_;
+		int numerator1 = frac.denominator_ * numerator_;
+		int numerator2 = denominator_ * frac.numerator_;
+		int numerator = numerator1 - numerator2;
+
+		return Fraction(numerator, stn);
 	}
 
-	Fraction operator*(Fraction other)
+	Fraction operator*(Fraction frac)
 	{
-		return Fraction(numerator_ * other.numerator_, denominator_ * other.denominator_);
-	}
-	Fraction operator*(int other)
-	{
-		return Fraction(numerator_ * other, denominator_);
-	}
-
-	Fraction operator/(Fraction other)
-	{
-		return Fraction(numerator_ / other.numerator_, denominator_ / other.denominator_);
-	}
-	Fraction operator/(int other)
-	{
-		return Fraction(numerator_ / other, denominator_);
+		int numerator = numerator_ * frac.numerator_;
+		int denominator = denominator_ * frac.denominator_;
+		
+		reduction(numerator, denominator);
+		return Fraction(numerator, denominator);
 	}
 
-	bool operator==(Fraction frac)
+	Fraction operator/(Fraction frac)
 	{
-		return Abs() == frac.Abs();
+		int numerator = numerator_ * frac.denominator_;
+		int denominator = denominator_ * frac.numerator_;
+
+		reduction(numerator, denominator);
+		return Fraction(numerator, denominator);
 	}
-	bool operator!=(Fraction frac)
+
+	Fraction operator-()
 	{
-		return !(*this == frac);
+		Fraction negated = *this;
+		negated.numerator_ *= -1;
+
+		return negated;
 	}
-	bool operator>(Fraction frac)
+
+	Fraction& operator++()
 	{
-		return Abs() > frac.Abs();
+		numerator_++;
+		denominator_++;
+		return *this;
 	}
-	bool operator<(Fraction frac)
+
+	Fraction operator++(int)
 	{
-		return frac > *this;
+		Fraction tmp = *this;
+		++(*this);
+		return tmp;
 	}
-	bool operator>=(Fraction frac)
+
+	Fraction& operator--()
 	{
-		return !(*this < frac);
+		numerator_--;
+		denominator_--;
+		return *this;
 	}
-	bool operator<=(Fraction frac)
+
+	Fraction operator--(int)
 	{
-		return !(*this > frac);
+		Fraction tmp = *this;
+		--(*this);
+		return tmp;
+	}
+
+	void print()
+	{
+		std::cout << numerator_ << "/" << denominator_ << std::endl;
 	}
 };
 
 int main()
 {
-	Fraction f1(4, 3);
-	Fraction f2(6, 11);
+	setlocale(LC_ALL, "Russian");
 
-	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
-	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
-	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
-	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
-	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
-	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
-	return 0;
+	int numerator1, denominator1;
+	int numerator2, denominator2;
+
+	std::cout << "Введите числитель дроби 1: ";
+	std::cin >> numerator1;
+	std::cout << "Введите знаменатель дроби 1: ";
+	std::cin >> denominator1;
+	std::cout << "Введите числитель дроби 2: ";
+	std::cin >> numerator2;
+	std::cout << "Введите знаменатель дроби 2: ";
+	std::cin >> denominator2;
+
+	Fraction f1(numerator1, denominator1);
+	Fraction f2(numerator2, denominator2);
+
+	Fraction add = f1 + f2;
+	Fraction sub = f1 - f2;
+	Fraction multi = f1 * f2;
+	Fraction divide = f1 / f2;
+	Fraction minusf1 = -f1;
+	Fraction minusf2 = -f2;
+	Fraction f1_pre = ++f1 * f2;
+	Fraction f2_pre = f1 * ++f2;
+	Fraction f1_post = f1++ * f2;
+	Fraction f2_post = f1 * f2++;
+	Fraction f1_pre_min = --f1 * f2;
+	Fraction f2_pre_min = f1 * --f2;
+	Fraction f1_post_min = f1-- * f2;
+	Fraction f2_post_min = f1 * f2--;
+
+	std::cout << numerator1 << "/" << denominator1 << " + " << numerator2 << "/" << denominator2 << " = ";
+	add.print();
+
+	std::cout << numerator1 << "/" << denominator1 << " - " << numerator2 << "/" << denominator2 << " = ";
+	sub.print();
+
+	std::cout << numerator1 << "/" << denominator1 << " * " << numerator2 << "/" << denominator2 << " = ";
+	multi.print();
+
+	std::cout << numerator1 << "/" << denominator1 << " / " << numerator2 << "/" << denominator2 << " = ";
+	divide.print();
+
+	std::cout << numerator1 << "/" << denominator1 << " унарный -" << " = ";
+	minusf1.print();
+
+	std::cout << numerator2 << "/" << denominator2 << " унарный -" << " = ";
+	minusf2.print();
+
+	std::cout << "++" << numerator1 << "/" << denominator1 << " * " << numerator2 << "/" << denominator2 << " = ";
+	f1_pre.print();
+
+	std::cout << "Значение дроби 1: ";
+	f1.print();
+
+	std::cout << numerator1 << "/" << denominator1 << "--" << " * " << numerator2 << "/" << denominator2 << " = ";
+	f1_post_min.print();
+
+	std::cout << "Значение дроби 1: ";
+	f1.print();
+
 }
-
