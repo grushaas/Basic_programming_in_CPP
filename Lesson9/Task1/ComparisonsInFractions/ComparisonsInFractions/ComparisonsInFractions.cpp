e84c30dd2bfb604e8c107cdebca996fac6aeee56
Grushaas <grusha_as@outlook.com>
2022-07-14 16:42:58 +0700

    Lesson 9 - Task 1 - Version 1.3 - Error with gcd() function

--- Lesson9/Task1/ComparisonsInFractions/ComparisonsInFractions/ComparisonsInFractions.cpp
+++ Lesson9/Task1/ComparisonsInFractions/ComparisonsInFractions/ComparisonsInFractions.cpp
@@ -1,10 +1,30 @@
 ﻿#include <iostream>
+#include <numeric>
 
 class Fraction
 {
 private:
 	int numerator_;
 	int denominator_;
+
+	//int gcd(int a, int b)
+	//{
+	//	return b ? a : gcd(b, a % b);
+	//}
+
+	void reduction(int& numerator_, int& denominator_, Fraction& frac)
+	{
+		int lcd = std::gcd(3, 11);
+
+		int multiplier1 = lcd / denominator_;
+		int multiplier2 = lcd / frac.denominator_;
+
+		numerator_ *= multiplier1;
+		denominator_ *= multiplier1;
+
+		frac.numerator_ *= multiplier2;
+		frac.denominator_ *= multiplier2;
+	}
 
 public:
 	Fraction(int numerator, int denominator)
@@ -20,12 +40,14 @@ public:
 
 	bool operator!=(Fraction frac)
 	{
-		return !(numerator_ == frac.numerator_ && denominator_ == frac.denominator_);
+		return !(*this == frac);
 	}
 
 	bool operator<(Fraction frac)
 	{
-		return (numerator_ < frac.numerator_) && (denominator_ < frac.denominator_);
+		reduction(numerator_, denominator_, frac);
+
+		return (numerator_ < frac.numerator_) && (denominator_ < denominator_);
 	}
 
 	bool operator>(Fraction frac)
